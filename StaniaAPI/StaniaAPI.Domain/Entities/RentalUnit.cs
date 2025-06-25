3f3bb79c9b6527cd2291897723a90d320ecbe87e
Dragisha-Todoroski <dade99mk@gmail.com>
2025-06-26 01:09:19 +0200

    Initial commit

---
+++ StaniaAPI/StaniaAPI.Domain/Entities/RentalUnit.cs
@@ -0,0 +1,38 @@
+﻿using StaniaAPI.Services.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaniaAPI.Domain.Entities
+{
+    public class RentalUnit
+    {
+        public Guid Id { get; set; }
+
+        // Categorization
+        public RentalUnitType RentalUnitType { get; set; }
+        public RentalUnitTerm RentalUnitTerm { get; set; }
+
+        // Common characteristics
+        public int BedroomCount { get; set; } = 0;
+        public int BathroomCount { get; set; } = 1;
+        public int SquareFootage { get; set; }
+
+        // Optional features
+        public int? NumOfFloors { get; set; } // primarily for houses/entire complexes
+        public int? FloorNumber { get; set; } // primarily for studios/apartments/rooms
+        public ParkingOption ParkingOption { get; set; }
+        public ParkingCost ParkingCost { get; set; }
+        public bool HasGarage { get; set; }
+        public bool HasGarden { get; set; }
+
+        // Location, pricing & description
+        public string Address { get; set; } = null!;
+        public decimal Price { get; set; }
+        public string Currency { get; set; } = "EUR";
+        public string? Description { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
---
+++ StaniaAPI/StaniaAPI.Services/Enums/ParkingOption.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaniaAPI.Services.Enums
+{
+    public enum ParkingOption
+    {
+        [Display(Name = "Street parking")]
+        StreetParking = 1,
+        Garage = 2,
+
+        [Display(Name = "No parking")]
+        NoParking = 3,
+
+        [Display(Name = "Parking lot")]
+        ParkingLot = 4,
+    }
+}
