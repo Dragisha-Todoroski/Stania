067d3a955d03fc16ebdcec58d44ed4ceec3fcc92
Daniel Cikora <d.cikora98@gmail.com>
2025-07-01 20:03:22 +0200

    Frontend - Basic Folder Structure Created, Global Styles Added.

--- Frontend/booking-app/components/layout/Header.tsx
+++ Frontend/booking-app/components/layout/Header.tsx
@@ -1,7 +1,11 @@
 export default function Header() {
   return (
-    <header>
-      <nav>nav</nav>
+    <header className="header">
+      <div className="wrapper">
+        <div className="header__content">
+          <h1 className="header__content-title text-4xl font-semibold">Booking App</h1>
+        </div>
+      </div>
     </header>
   );
 }
---
+++ Frontend/booking-app/components/ui/Button.tsx
@@ -0,0 +1,3 @@
+export default function Button () {
+    return <button type='' aria-label="Button">Button</button>
+}
