09c321812c31b5e5256c98ebaf2303442d5145d6
Dragisha-Todoroski <dade99mk@gmail.com>
2025-08-07 18:56:34 +0200

    Backend - Created IBaseCrudRepository and IRentalUnitRepository which extends it.

---
+++ Backend/StaniaAPI.Repositories/Abstractions/IBaseCrudRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaniaAPI.Repositories.Abstractions
+{
+    public interface IBaseCrudRepository<TEntity>
+    {
+        Task<IEnumerable<TEntity>> GetAllAsync();
+        Task<TEntity?> GetByIdAsync(Guid id);
+        Task<TEntity> CreateAsync(TEntity entity);
+        Task<TEntity> UpdateAsync(TEntity entity);
+        Task<bool> DeleteAsync(TEntity entity);
+    }
+}
---
+++ Backend/StaniaAPI.Repositories/Abstractions/RentalUnitAbstractions/IRentalUnitRepository.cs
@@ -0,0 +1,14 @@
+﻿using StaniaAPI.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaniaAPI.Repositories.Abstractions.RentalUnitAbstractions
+{
+    public interface IRentalUnitRepository : IBaseCrudRepository<RentalUnit>
+    {
+        
+    }
+}
