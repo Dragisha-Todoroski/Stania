fb99e21cb6622a18717fdbda7765c4b02d88b6f5
Dragisha-Todoroski <dade99mk@gmail.com>
2025-08-08 21:05:57 +0200

    Backend - Created IRegionRepository interface. Created RegionRepository methods for DB manipulation.

---
+++ Backend/StaniaAPI.Repositories/Abstractions/RegionAbstractions/IRegionRepository.cs
@@ -0,0 +1,13 @@
+﻿using StaniaAPI.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaniaAPI.Repositories.Abstractions.RegionAbstractions
+{
+    public interface IRegionRepository : IBaseCrudRepository<Region>
+    {
+    }
+}
---
+++ Backend/StaniaAPI.Repositories/Implementations/RegionRepository/RegionRepository.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StaniaAPI.DataAccess;
+using StaniaAPI.Domain.Entities;
+using StaniaAPI.Repositories.Abstractions.RegionAbstractions;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaniaAPI.Repositories.Implementations.RegionRepository
+{
+    public class RegionRepository : IRegionRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public RegionRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Region>> GetAllAsync()
+        {
+            var regionList = await _context.Set<Region>().ToListAsync();
+
+            return regionList;
+        }
+
+        public async Task<Region?> GetByIdAsync(Guid id)
+        {
+            var region = await _context.Set<Region>().FindAsync(id);
+
+            return region;
+        }
+
+        public async Task<Region> CreateAsync(Region region)
+        {
+            await _context.Set<Region>().AddAsync(region);
+            await _context.SaveChangesAsync();
+
+            return region;
+        }
+
+        public async Task<Region?> UpdateAsync(Region region)
+        {
+            var dbRegion = await _context.Set<Region>().FindAsync(region.Id);
+
+            // Should never trigger with GetByIdAsync being called prior in the Services layer (therefore no double null check there)
+            if (dbRegion == null)
+                return null;
+
+            _context.Entry(dbRegion).CurrentValues.SetValues(region);
+            await _context.SaveChangesAsync();
+
+            return dbRegion;
+        }
+
+        public async Task<bool> DeleteAsync(Guid id)
+        {
+            var dbRegion = await _context.Set<Region>().FindAsync(id);
+
+            if (dbRegion == null)
+                return false;
+
+            _context.Set<Region>().Remove(dbRegion);
+
+            return await _context.SaveChangesAsync() > 0;
+        }
+    }
+}
