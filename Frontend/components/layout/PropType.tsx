cf85bdc3e2dee5a8cee6dfd68c39faa3fbab567f
TeodoraDumitru99 <teodum.1999@gmail.com>
2025-08-12 16:06:04 +0300

    PropType - className changed to match Section Name

--- Frontend/components/layout/PropType.tsx
+++ Frontend/components/layout/PropType.tsx
@@ -26,29 +26,29 @@ export default function PropType() {
     },
   ];
   return (
-    <section className="topprop my-10">
+    <section className="proptype my-10">
       <div className="wrapper">
-        <div className="toprop_content flex flex-col gap-10">
-          <div className="toprpop_header flex flex-col gap-3">
+        <div className="proptype_content flex flex-col gap-10">
+          <div className="proptype_header flex flex-col gap-3">
             <h3 className="text-gray-800 lg:text-4xl md:text-3xl xs:text-2xl text-xl font-semibold">
               Search by property type
             </h3>
           </div>
 
-          <div className="topprop_list grid lg:grid-cols-4 md:gap-6 xs:grid-cols-2 gap-4 grid-cols-1">
+          <div className="proptype_list grid lg:grid-cols-4 md:gap-6 xs:grid-cols-2 gap-4 grid-cols-1">
             {listContent.map(
               ({ id, imgsrc, alt, title}) => (
                 <div
-                  className="topprop_list_item  rounded-xl overflow-clip h-max w-auto drop-shadow-md"
+                  className="proptype_list_item  rounded-xl overflow-clip h-max w-auto drop-shadow-md"
                   key={id}
                 >
                   <img
                     loading="lazy"
                     src={imgsrc}
                     alt={alt}
-                    className="projects_element_image w-full"
+                    className="proptype_element_image w-full"
                   />
-                  <div className="toprpop_item_text xs:px-6 xs:pb-8 xs:pt-4 px-4 pb-6 pt-2 h-fill bg-[#222222] flex flex-col gap-3">
+                  <div className="proptype_item_text xs:px-6 xs:pb-8 xs:pt-4 px-4 pb-6 pt-2 h-fill bg-[#222222] flex flex-col gap-3">
                     <h5 className="lg:text-2xl md:text-xl xs:text-lg font-bold text-gray-50">
                       {title}
                     </h5>
